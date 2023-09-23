9603b5b07be6017b07467a0b86ce7cf8d2cce87d
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2023-09-23 07:39:57 +0700

    setup vuetify plugins and integrated with nuxt config

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-100 h-screen d-flex justify-center align-center">
+    <v-card class="pa-8">
+    Hello World!
+    </v-card>
   </div>
 </template>
